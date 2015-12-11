a0c7e27099fa66a682eb4e9a147ac075be6452ee
Pat Hickey <pat@moreproductive.org>
2015-12-11 15:17:20 -0800

    add git config script

---
+++ files/git_config.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+set -e
+git config --global user.name "Pat Hickey"
+git config --global user.email "pat@moreproductive.org"
+git config --global push.default simple
+git config --global core.editor $(which vim)
+git config --global core.excludesfile $HOME/.gitignore_global
+git config --global color.ui true
+git config --global log.decorate full
+git config --global url.ssh://git@github.com:.insteadOf https://git@github.com
+git config --global alias.unstage 'reset HEAD --'
+git config --global alias.co 'commit -v'
+git config --global alias.l 'log --stat'
+git config --global alias.last 'log -1 HEAD'
