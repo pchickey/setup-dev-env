6b1bf2f817840625fb522e86386225b2e5160d63
Pat Hickey <pat@moreproductive.org>
2015-12-14 11:58:13 -0800

    git config bugfix

--- files/git_config.sh
+++ files/git_config.sh
@@ -7,7 +7,7 @@ git config --global core.editor $(which vim)
 git config --global core.excludesfile $HOME/.gitignore_global
 git config --global color.ui true
 git config --global log.decorate full
-git config --global url.ssh://git@github.com:.insteadOf https://git@github.com
+git config --global url.ssh://git@github.com:.insteadOf https://github.com
 git config --global alias.unstage 'reset HEAD --'
 git config --global alias.co 'commit -v'
 git config --global alias.l 'log --stat'
