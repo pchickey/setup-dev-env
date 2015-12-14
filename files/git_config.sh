30ab6c1bd599e099ea440bba180bbf06303ef63b
Pat Hickey <pat@moreproductive.org>
2015-12-14 15:53:04 -0800

    git config: fix gitignore global

--- files/git_config.sh
+++ files/git_config.sh
@@ -4,7 +4,7 @@ git config --global user.name "Pat Hickey"
 git config --global user.email "pat@moreproductive.org"
 git config --global push.default simple
 git config --global core.editor $(which vim)
-git config --global core.excludesfile $HOME/.gitignore_global
+git config --global core.excludesfile $HOME/.dotfiles/.gitignore_global
 git config --global color.ui true
 git config --global log.decorate full
 git config --global url.ssh://git@github.com:.insteadOf https://github.com
