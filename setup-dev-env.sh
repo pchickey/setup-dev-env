c50b15cfcc5524dd18d6c6b5e45c3304e5b923b2
Pat Hickey <pat@moreproductive.org>
2020-12-09 21:29:19 -0800

    install some more stuff

--- setup-dev-env.sh
+++ setup-dev-env.sh
@@ -1,113 +1,123 @@
 #!/usr/bin/env bash
 set -ex
 
-mkdir -p $HOME/.local/bin
-export PATH=$PATH:$HOME/.local/bin
+if [ ! -d "$HOME/.local/bin" ]; then
+    mkdir -p $HOME/.local/bin
+    export PATH=$PATH:$HOME/.local/bin
+fi
 
-sudo apt install \
-	build-essential \
-	vim \
-	git \
-	tmux \
-	zsh \
-	clang \
-	curl \
-	cmake \
-	bison \
-	flex \
-	ninja-build \
-	autoconf \
-	pkg-config \
-	libevent-dev \
-	libncurses-dev \
-	gitk \
-	tree \
-	xclip \
-	arandr \
-	feh \
-    scrot
+if [ ! $(command -v make) ]; then
+    sudo apt install \
+        build-essential \
+        vim \
+        git \
+        tmux \
+        zsh \
+        clang \
+        curl \
+        cmake \
+        bison \
+        flex \
+        ninja-build \
+        autoconf \
+        pkg-config \
+        libevent-dev \
+        libncurses-dev \
+        gitk \
+        tree \
+        xclip \
+        arandr \
+        feh \
+        scrot
+fi
 
 if [ ! -d "$HOME/.zsh" ]; then
-	ln -s $PWD/dotfiles/zsh $HOME/.zsh
-	ln -s $HOME/.zsh/zshrc $HOME/.zshrc
+    ln -s $PWD/dotfiles/zsh $HOME/.zsh
+    ln -s $HOME/.zsh/zshrc $HOME/.zshrc
 fi
 
 
 
 if [ ! -f "$HOME/.tmux.conf" ]; then
-	if [ "tmux 2.5" == $(tmux -V) ] || [ "tmux master" == $(tmux -V)] ; then
-		echo "Correct version of tmux installed, linking conf file"
-	else
-		echo "Incompatible version of tmux installed: " $(tmux -V)
-		if [ ! -d "tmux" ]; then
-			git clone https://github.com/tmux/tmux.git
-		fi
-		pushd tmux
-		sh autogen.sh
-		./configure
-		make
-		sudo make install
-		echo "Master version of tmux has been installed"
-		popd
-	fi
-	ln -s $PWD/dotfiles/tmux.conf $HOME/.tmux.conf
+    if [ "tmux 2.5" == $(tmux -V) ] || [ "tmux master" == $(tmux -V)] ; then
+        echo "Correct version of tmux installed, linking conf file"
+    else
+        echo "Incompatible version of tmux installed: " $(tmux -V)
+        if [ ! -d "tmux" ]; then
+            git clone https://github.com/tmux/tmux.git
+        fi
+        pushd tmux
+        sh autogen.sh
+        ./configure
+        make
+        sudo make install
+        echo "Master version of tmux has been installed"
+        popd
+    fi
+    ln -s $PWD/dotfiles/tmux.conf $HOME/.tmux.conf
+fi
+
+if [ ! -f $PWD/nvim.appimage ]; then
+    curl -sSfLO https://github.com/neovim/neovim/releases/download/v0.4.4/nvim.appimage
+    chmod +x nvim.appimage
+    ln -s $PWD/nvim.appimage $HOME/.local/bin/nvim
 fi
 
 
 if [ ! -f "$HOME/.config/nvim/init.vim" ]; then
-	mkdir -p $HOME/.config
-	ln -s $PWD/dotfiles/nvim $HOME/.config/nvim
+    mkdir -p $HOME/.config
+    ln -s $PWD/dotfiles/nvim $HOME/.config/nvim
 fi
 
 if [ ! -f "$HOME/.ssh/config" ]; then
-	echo "AddKeysToAgent yes" >> $HOME/.ssh/config
+    echo "AddKeysToAgent yes" >> $HOME/.ssh/config
 fi
 
 if [ ! -f "$HOME/.ssh/rc" ]; then
-	echo "if test \"\$SSH_AUTH_SOCK\" ; then" >> $HOME/.ssh/rc
-	echo "  ln -sf \$SSH_AUTH_SOCK $HOME/.ssh/ssh_auth_sock" >> $HOME/.ssh/rc
-	echo "fi" >> $HOME/.ssh/rc
+    echo "if test \"\$SSH_AUTH_SOCK\" ; then" >> $HOME/.ssh/rc
+    echo "  ln -sf \$SSH_AUTH_SOCK $HOME/.ssh/ssh_auth_sock" >> $HOME/.ssh/rc
+    echo "fi" >> $HOME/.ssh/rc
 fi
 
 if [ ! -f "$HOME/.cargo/bin/rustc" ] ; then
-	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
     export PATH=$HOME/.cargo/bin:$PATH
-	rustup component add rls --toolchain stable-x86_64-unknown-linux-gnu
-	rustup component add rustfmt
+    rustup component add rustfmt
+    rustup component add rust-src
 fi
 
 if [ ! -f "$HOME/.cargo/bin/rg" ] ; then
-	$HOME/.cargo/bin/cargo install ripgrep
+    $HOME/.cargo/bin/cargo install ripgrep
 fi
 
 # LanguageClient-neovim depends on rust
 if [ ! -d "$HOME/src/LanguageClient-neovim" ]; then
-	mkdir -p $HOME/src
-	pushd $HOME/src
-	git clone https://github.com/autozimu/LanguageClient-neovim
-	cd LanguageClient-neovim
-	export PATH=$HOME/.cargo/bin:$PATH
-	make release
-	popd
+    mkdir -p $HOME/src
+    pushd $HOME/src
+    git clone https://github.com/autozimu/LanguageClient-neovim
+    cd LanguageClient-neovim
+    export PATH=$HOME/.cargo/bin:$PATH
+    make release
+    popd
 fi
 
 if [[ ! $(git config --global user.email) == "pat@moreproductive.org" ]]; then
-	git config --global user.name "Pat Hickey"
-	git config --global user.email "pat@moreproductive.org"
-	git config --global push.default simple
-	git config --global core.editor $(which vim)
-	git config --global core.excludesfile $HOME/.dotfiles/gitignore_global
-	git config --global color.ui true
-	git config --global log.decorate full
-	git config --global alias.unstage 'reset HEAD --'
-	git config --global alias.co 'commit -v'
-	git config --global alias.l 'log --stat'
-	git config --global alias.last 'log -1 HEAD'
-	git config --global alias.sms 'submodule status --recursive'
-	git config --global alias.smu 'submodule update --init --recursive'
-	git config --global merge.conflictstyle 'diff3'
-	git config --global diff.submodule log
-	git config --global status.submodulesummary 1
+    git config --global user.name "Pat Hickey"
+    git config --global user.email "pat@moreproductive.org"
+    git config --global push.default simple
+    git config --global core.editor $(which vim)
+    git config --global core.excludesfile $HOME/.dotfiles/gitignore_global
+    git config --global color.ui true
+    git config --global log.decorate full
+    git config --global alias.unstage 'reset HEAD --'
+    git config --global alias.co 'commit -v'
+    git config --global alias.l 'log --stat'
+    git config --global alias.last 'log -1 HEAD'
+    git config --global alias.sms 'submodule status --recursive'
+    git config --global alias.smu 'submodule update --init --recursive'
+    git config --global merge.conflictstyle 'diff3'
+    git config --global diff.submodule log
+    git config --global status.submodulesummary 1
 fi
 if [ ! -d $HOME/.config/git/template ]; then
     mkdir -p $HOME/.config/git/template
@@ -116,26 +126,26 @@ if [ ! -d $HOME/.config/git/template ]; then
 fi
 
 if [ ! -d $HOME/.fzf ]; then
-	pushd $HOME
-	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-	~/.fzf/install
-	popd
+    pushd $HOME
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install
+    popd
 fi
 
 if [ $(command -v dconf) ]; then
-	dconf write /org/gnome/desktop/input-sources/xkb-options "['ctrl:nocaps']"
+    dconf write /org/gnome/desktop/input-sources/xkb-options "['ctrl:nocaps']"
 fi
 
 if [ ! $(command -v alacritty) ]; then
     # For XPS 15 I ended up having to install from source
-	sudo apt install \
+    sudo apt install \
         libfreetype6-dev \
         libexpat1-dev \
         libxcb1-dev \
         libxcb-render0-dev \
         libxcb-shape0-dev \
         libxcb-xfixes0-dev \
-	libfontconfig1-dev
+    libfontconfig1-dev
     mkdir -p $HOME/src
     pushd $HOME/src
     git clone https://github.com/alacritty/alacritty
@@ -147,8 +157,13 @@ if [ ! $(command -v alacritty) ]; then
 fi
 
 if [ ! -d $HOME/.config/i3 ]; then
-	sudo apt install i3
-	ln -s $PWD/dotfiles/i3 $HOME/.config/i3
+    sudo apt install i3
+    ln -s $PWD/dotfiles/i3 $HOME/.config/i3
+    if [ ! -d $HOME/.screenlayout ]; then
+        mkdir $HOME/.screenlayout
+        touch $HOME/.screenlayout/home.sh
+        chmod +x $HOME/.screenlayout/home.sh
+    fi
 fi
 
 
@@ -159,22 +174,16 @@ if [ ! -f /etc/systemd/system/i3lock.service ]; then
 fi
 
 if [ ! -d $HOME/.fonts ]; then
-	mkdir -p $HOME/.fonts
-	curl -sSfLO https://download.jetbrains.com/fonts/JetBrainsMono-1.0.2.zip
-	unzip JetBrainsMono-1.0.2.zip
-	for f in $PWD/JetBrainsMono-1.0.2/ttf/*.ttf
-	do
-		mv $f $HOME/.fonts/$(basename "$f")
-	done
-	rm -rf $PWD/JetBrainsMono-1.0.2
-	rm JetBrainsMono-1.0.2.zip
-	sudo fc-cache -f -v
-fi
-
-if [ ! -f $PWD/nvim.appimage ]; then
-	curl -sSfLO https://github.com/neovim/neovim/releases/download/v0.4.4/nvim.appimage
-	chmod +x nvim.appimage
-	ln -s $PWD/nvim.appimage $HOME/.local/bin/nvim
+    mkdir -p $HOME/.fonts
+    curl -sSfLO https://download.jetbrains.com/fonts/JetBrainsMono-1.0.2.zip
+    unzip JetBrainsMono-1.0.2.zip
+    for f in $PWD/JetBrainsMono-1.0.2/ttf/*.ttf
+    do
+        mv $f $HOME/.fonts/$(basename "$f")
+    done
+    rm -rf $PWD/JetBrainsMono-1.0.2
+    rm JetBrainsMono-1.0.2.zip
+    sudo fc-cache -f -v
 fi
 
 if [ ! -d $HOME/src/binaryen ]; then
@@ -186,7 +195,6 @@ if [ ! -d $HOME/src/binaryen ]; then
     cd build
     cmake -G Ninja ..
     ninja
-    mkdir -p $HOME/.local/bin
     for f in $HOME/src/binaryen/build/bin/*
     do
         ln -s $f $HOME/.local/bin/$(basename "$f")
@@ -211,3 +219,17 @@ if [ ! -d $HOME/src/wabt ]; then
 fi
 
 
+if [ ! $(command -v rust-analyzer) ]; then
+    curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-linux \
+        -o $HOME/.local/bin/rust-analyzer
+    chmod +x $HOME/.local/bin/rust-analyzer
+fi
+
+
+if [ ! $(command -v sccache) ]; then
+    sudo apt install libssl-dev
+    cargo install sccache
+    echo "[build]" >> $HOME/.cargo/config
+    echo "rustc-wrapper = \"$HOME/.cargo/bin/sccache\"" >> $HOME/.cargo/config
+    echo "incremental = false" >> $HOME/.cargo/config
+fi
