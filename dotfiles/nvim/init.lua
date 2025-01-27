b443ee1a62ddb3353429d8cc7c7f346cd1bcdc23
Pat Hickey <p.hickey@f5.com>
2025-01-26 16:17:03 -0800

    nvim fix for weird rust-analyzer behavior

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -129,11 +129,22 @@ nvim_lsp.rust_analyzer.setup({
                 enable = true
             },
             diagnostics = {
-                disabled = { "inactive-code" }
+                disabled = { "inactive-code" },
             },
             procMacro = {
                 enable = true
-            }
+            },
         }
     }
 })
+
+-- rust-analyzer started giving these errors that nvim isnt handling correctly??? https://github.com/neovim/neovim/issues/30985
+for _, method in ipairs({ "textDocument/diagnostic", "workspace/diagnostic" }) do
+    local default_diagnostic_handler = vim.lsp.handlers[method]
+    vim.lsp.handlers[method] = function(err, result, context, config)
+        if err ~= nil and err.code == -32802 then
+            return
+        end
+        return default_diagnostic_handler(err, result, context, config)
+    end
+end
