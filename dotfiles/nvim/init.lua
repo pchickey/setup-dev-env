7811520f169a910b183cc3a70b4156a32ba333fc
Pat Hickey <p.hickey@f5.com>
2025-08-22 16:18:59 -0700

    nvim: remove workaround for bug fixed by v0.10.3

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -114,15 +114,3 @@ vim.api.nvim_create_autocmd("LspAttach", {
     })
   end
 })
-
-
--- rust-analyzer started giving these errors that nvim isnt handling correctly??? https://github.com/neovim/neovim/issues/30985
-for _, method in ipairs({ "textDocument/diagnostic", "workspace/diagnostic" }) do
-    local default_diagnostic_handler = vim.lsp.handlers[method]
-    vim.lsp.handlers[method] = function(err, result, context, config)
-        if err ~= nil and err.code == -32802 then
-            return
-        end
-        return default_diagnostic_handler(err, result, context, config)
-    end
-end
